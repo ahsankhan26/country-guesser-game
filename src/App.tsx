8307c01cfb02e7a65736928582ab80a95970a3ef
Ahsan Khan <ahsan@geeksofkolachi.com>
2022-07-31 18:03:20 +0500

    add map and input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, FormEvent } from 'react';
+import WorldMap from 'react-svg-worldmap';
 import './App.css';
+import { countryNames, countryCodes } from './countries';
 
-function App() {
+const App: React.FC = () => {
+  const [data, setData] = useState<{ country: string; value: number }[]>([
+    { country: '', value: 0 },
+  ]);
+
+  const correctGuess = (countryName: string) => {
+    const countryCode = countryCodes[countryName as keyof typeof countryCodes];
+    const dataCopy = [...data];
+
+    if (Array.isArray(countryCode)) {
+      countryCode.forEach((item) => {
+        dataCopy.push({ country: item, value: 1 });
+      });
+    } else {
+      const temp = { country: countryCode, value: 1 };
+      dataCopy.push(temp);
+    }
+    setData(dataCopy);
+  };
+
+  const checkCountry = (e: FormEvent) => {
+    const target = e.target as HTMLInputElement;
+
+    const name = countryNames.find(
+      (item) => item === target.value.toUpperCase()
+    );
+    if (name) {
+      correctGuess(name);
+      target.value = '';
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <div>
+        <label
+          htmlFor='countryName'
+          style={{
+            display: 'block',
+            marginBottom: 2,
+            fontSize: 'small',
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Country Name
+        </label>
+        <input
+          type='text'
+          id='countryName'
+          placeholder='Country Name'
+          onInput={checkCountry}
+        />
+      </div>
+      <WorldMap color='#0099ff' size='xl' data={data} richInteraction frame />
     </div>
   );
-}
+};
 
 export default App;
